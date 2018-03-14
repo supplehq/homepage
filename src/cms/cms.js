85450a12c09a1f1568e6a561ea5ce267418f70a8
Seunggeun Song <seunggeun@nector.co>
2018-03-14 21:51:30 +0900

    Add preview css in development

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,11 @@
 import CMS from 'netlify-cms'
 
+import stylesURL from '!file-loader?name=[hash].css!sass-loader!../layouts/all.sass'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
-CMS.registerPreviewStyle('/styles.css')
+CMS.registerPreviewStyle(stylesURL)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
